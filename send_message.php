1679dc851c95d0681610431592a570fe4514b38a
Valb.Mig <valb.mig@gmail.com>
2023-08-22 09:38:49 -0300

    [Organizando Projeto - Me paga :)]

--- send_message.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_POST['message'])) {
-    $message = $_POST['message'];
-    $filename = 'messages.txt';
-    file_put_contents($filename, $message . PHP_EOL, FILE_APPEND);
-    echo "Mensagem enviada com sucesso!";
-}
-?>
-
